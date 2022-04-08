0d2de21bdd1d97abcd74e04e827381bdab6079db
Andrey <andreq1293@gmail.com>
2022-04-08 12:42:51 +0300

    настройка сборщика

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,13 @@ plugins {
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
+    id("com.github.johnrengelman.processes") version "0.5.0"
+    id("org.springdoc.openapi-gradle-plugin") version "1.3.3"
+    id("nu.studer.jooq") version "6.0.1"
 }
 
 group = "ru.alexandra_incr"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
@@ -26,6 +29,12 @@ dependencies {
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
+    testImplementation("org.springdoc:springdoc-openapi-webmvc-core:1.6.6")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.6.6")
+    implementation("org.springdoc:springdoc-openapi-security:1.6.6")
+    implementation("com.auth0:java-jwt:3.18.3")
 }
 
 tasks.withType<KotlinCompile> {
@@ -35,6 +44,55 @@ tasks.withType<KotlinCompile> {
     }
 }
 
+jooq{
+    configurations {
+        create("main") {
+            generateSchemaSourceOnCompilation.set(false)
+
+            jooqConfiguration.apply {
+                jdbc.apply {
+                    driver = "org.postgresql.Driver"
+                    url = "jdbc:postgresql://localhost:5432/database"
+                    user = "postgres"
+                    password = "5213"
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.DefaultGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                        inputSchema = "public"
+                        outputSchema = ""
+                        forcedTypes.addAll(
+                            arrayOf(
+                                org.jooq.meta.jaxb.ForcedType()
+                                    .withName("varchar")
+                                    .withIncludeExpression(".*")
+                                    .withIncludeTypes("INET")
+                            ).toList()
+                        )
+                    }
+                    generate.apply {
+                        isPojosEqualsAndHashCode = true
+                        isDeprecated = false
+                        isRecords = true
+                        isImmutablePojos = true
+                        isFluentSetters = true
+                    }
+
+                    target.apply {
+                        directory = "${projectDir}/src/main/kotlin/jooq/"  // default (can be omitted)
+                    }
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+                }
+            }
+        }
+    }
+}
+
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.named<nu.studer.gradle.jooq.JooqGenerate>("generateJooq") {
+    allInputsDeclared.set(true)
 }
