5179a094e9c3dc7751280d70f9aab82723849b43
Andrey <andreq1293@gmail.com>
2022-04-08 11:49:29 +0300

    Initial commit

---
+++ src/main/kotlin/ru/alexandra_incr/authorssupervision/AuthorsSupervisionApplication.kt
@@ -0,0 +1,11 @@
+package ru.alexandra_incr.authorssupervision
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthorsSupervisionApplication
+
+fun main(args: Array<String>) {
+    runApplication<AuthorsSupervisionApplication>(*args)
+}
